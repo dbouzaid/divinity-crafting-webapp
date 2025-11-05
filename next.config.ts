9cb6bcef84ab245b156385914e4e9009efa47665
Daniel Bouzaid <daniel.bouzaid@avanade.com>
2025-11-05 17:57:59 +1100

    Removed config line

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ protocol: "https", hostname: "placehold.co" }],
   },
-
-  contentDispositionType: "attachment",
   contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
 };
 
